006960783753c89e20ded55033924a17fc48af55
kiplangatmike <m.kiplangat@alustudent.com>
2024-02-04 12:27:03 +0200

    initial commit

---
+++ var_let_const.js
@@ -0,0 +1,37 @@
+// var vs let vs const
+// scope
+
+// var is a global scope
+var a = 5;
+console.log(a)
+
+// ---------------------------------------
+// let and const is a block scope, it can only be accessed in the block, below is an example
+
+{
+    let c = 5;
+    console.log(c)
+}
+
+// -----------------------------------------
+// variable shadowing
+function test() {
+    let shadow = "Hello";
+    if (true) {
+        // this let shadows the first let
+        let shadow = "Hi";
+        console.log(shadow)
+    }
+    console.log(shadow);
+}
+test();
+// we cannot shadow let  by using var, this is called illigal shadowing
+
+// ---------------------------------------------
+
+// we can redeclare a variable using var in the same scope bit not let and const
+// when declaring const we need to initialize it with some value as opposed to var and let
+// we can reinitialize a value in let and var but not const
+
+// -----------------------------------------------
+
